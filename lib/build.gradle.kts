931be1061d9a257416bdd9a5e7ca13ec50cf66d6
Konstantin <konstantin@headbright.be>
2025-01-24 13:53:09 +0100

    chore: fix test dependencies

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -91,6 +91,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.androidx.jUnitTestRules)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
