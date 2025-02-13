7ac4ac8f51dd766b62ed68c8aa66208e658f954c
Konstantin <konstantin@headbright.be>
2025-02-13 16:36:27 +0100

    fix: fix warnings by opting into specific APIs

--- lib/src/main/java/com/telemetrydeck/sdk/UUIDSerializer.kt
+++ lib/src/main/java/com/telemetrydeck/sdk/UUIDSerializer.kt
@@ -1,6 +1,7 @@
 package com.telemetrydeck.sdk
 
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
 import kotlinx.serialization.encoding.Decoder
@@ -8,6 +9,7 @@ import kotlinx.serialization.encoding.Encoder
 import java.util.UUID
 
 
+@OptIn(ExperimentalSerializationApi::class)
 @Serializer(forClass = UUID::class)
 internal object UUIDSerializer : KSerializer<UUID> {
     override fun serialize(encoder: Encoder, value: UUID) {
